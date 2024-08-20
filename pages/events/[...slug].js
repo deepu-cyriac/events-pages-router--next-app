816323a144f8d1e36beaa127abfef5d59e8f3c37
Deepu Cyriac <deepucvk123@gmail.com>
2024-08-20 06:53:57 +0530

    Added the required routes

---
+++ pages/events/[...slug].js
@@ -0,0 +1,11 @@
+//this page is taken if there is more than one parameter after /events then this will be taken
+//if there is one, eventId.js will be taken
+//if there is nothing after events, index.js will be taken
+
+export default function FilteredEventsPage() {
+  return (
+    <div>
+      <h1>Filtered Events</h1>
+    </div>
+  );
+}
---
+++ pages/events/[eventId].js
@@ -0,0 +1,10 @@
+//this file will be taken if there is something after /events
+//else index.js will be taken
+
+export default function EventDetailPage() {
+  return (
+    <div>
+      <h1>Event Detail Page</h1>
+    </div>
+  );
+}
