b3b2d682b3afc0300e37552f8a59598191fac2d5
Deepu Cyriac <deepucvk123@gmail.com>
2024-08-20 07:22:38 +0530

    Added react components for loading and displaying event data

---
+++ components/events/event-item.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+
+export default function EventItem(props) {
+  const { title, image, date, location, id } = props;
+
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  const formattedAddress = location.replace(",", "\n");
+
+  const exploreLink = `/event/${id}`;
+
+  return (
+    <li>
+      {/* public folder will be available at / in next by default */}
+      <img src={"/" + image} alt={title} />
+      <div>
+        <div>
+          <h2>{title}</h2>
+          <div>
+            <time>{humanReadableDate}</time>
+          </div>
+          <div>
+            <address>{formattedAddress}</address>
+          </div>
+        </div>
+        <div>
+          <Link href={exploreLink}>Explore Event</Link>
+        </div>
+      </div>
+    </li>
+  );
+}
